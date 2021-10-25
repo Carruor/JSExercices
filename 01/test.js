7f1caf76ca844682d2a85500c1ab7916f91763b7
Carruor <fotoes10@gmail.com>
2021-10-25 11:17:28 +0200

    correccion 01

--- 01/test.js
+++ 01/test.js
@@ -1,10 +1,6 @@
 let group =["pepe","tomas", "laura","ana"]
-¨
-function PrintG(){
 
-for (let index - 0; index < group.length; index++) {
-    console.log(grupo[index])
-}
-}
-    
-PrintG();
+for(var i = 0; i < group. length ; i++){
+  
+    console. log(group[i])
+    }
